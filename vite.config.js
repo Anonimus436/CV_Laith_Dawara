cad0cb16e1cbc7197b484a6465f75a0d9fb0cc0a
Raad Raad <raad20011127@gmail.com>
2026-08-02 15:41:18 +0300

    CV

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,35 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     babel({ presets: [reactCompilerPreset()] }),
+//     tailwindcss()
+//   ],
+//    base: mode === 'production' ? '/CV_Laith_Dawara/' : '/',
+//   build: {
+//     outDir: 'dist'   
+//   }
+// })
+// إذا كنت تستخدم React:
+
+export default defineConfig(({ command, mode }) => { // هنا يتم تعريف 'mode'
+  return {
+    // هنا يأتي الجزء الذي أضفته
+    base: mode === 'production' ? '/CV_Laith_Dawara/' : '/',
+
+    build: {
+      outDir: 'dist' // مجلد الناتج الافتراضي
+    },
+
+     plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss()
-  ],
-   base: mode === 'production' ? '/CV_Laith_Dawara/' : '/',
-  build: {
-    outDir: 'dist' // مجلد الناتج الافتراضي
-  }
-})
+  ]
+    // هنا يمكنك إضافة Plugins الخاصة بك، مثلاً:
+    // plugins: [react()], // لـ React
+    // plugins: [vue()],   // لـ Vue
+  };
+});
