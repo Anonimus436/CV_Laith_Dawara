007a8323474af2307cb5bc5a2cdb111cc3ba3d94
Raad Raad <raad20011127@gmail.com>
2026-08-02 15:11:54 +0300

    CV

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,8 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss()
   ],
+   base: mode === 'production' ? '/CV_Laith_Dawara/' : '/',
+  build: {
+    outDir: 'dist' // مجلد الناتج الافتراضي
+  }
 })
